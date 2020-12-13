8bf3cf64d3fd670c676bf98f34717b5ed6463f24
ebhargro <75944377+ebhargro@users.noreply.github.com>
2020-12-13 13:25:55 -0500

    Add files via upload

---
+++ home.js
@@ -0,0 +1,35 @@
+var darkModeButton = document.getElementById("darkModeButton");
+
+darkModeButton.addEventListener("click", darkMode);
+
+function darkMode () {
+    document.body.style.backgroundColor = "black";
+    document.body.style.color= "white";
+
+}
+var lightModeButton = document.getElementById("lightModeButton");
+
+lightModeButton.addEventListener("click", lightMode);
+
+function lightMode() {
+    document.body.style.backgroundColor = "rgb(239,225,244)";
+    document.body.style.color="black";
+}
+//* Create a variable for the "Sign up" button//*
+
+var signUpButton = document.getElementById("signUpButton");
+
+//* Create a variable for what the user enters in response to the "What topic are you interested in?" question*//
+
+
+
+//* When user clicks sign up button, a message appears thanking them for signing up and displaying the subject area they were interested in//*
+
+signUpButton.addEventListener("click", signedUp);
+
+function signedUp () {
+    var firstName = document.getElementById("fName").value;
+    var lastName = document.getElementById("lName").value;
+    var userTopic = document.getElementById("userInterest").value;
+    alert("Thank you " + firstName + " " + lastName + "! " + "We will send you wellness resources related to " + userTopic + ".");
+}
